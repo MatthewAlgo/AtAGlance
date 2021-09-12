7a102f158226e9715994813ae8724faf3a1de2d0
Matthew_Algo <54024286+MatthewAlgo@users.noreply.github.com>
2021-09-12 18:42:17 +0300

    Minor Bug Fixed -> App crashed sometimes at recyclerview in newsHeadlines

--- app/src/main/java/app/matthewsgalaxy/ataglance/AdapterClasses/RecyclerViewHeadlinesAdapter.java
+++ app/src/main/java/app/matthewsgalaxy/ataglance/AdapterClasses/RecyclerViewHeadlinesAdapter.java
@@ -18,6 +18,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -66,26 +67,42 @@ public class RecyclerViewHeadlinesAdapter extends RecyclerView.Adapter<RecyclerV
     public void onBindViewHolder(@NonNull RecyclerViewHeadlinesAdapter.ViewHolder holder, int position) {
         Log.d(TAG, "onBindViewHolder: called");
         // Load The Features
-
-        // Modify The Text for Titles
-        holder.ChipNewsTitle.setText(TitlesArrayList.get(position));
-
-        // Modify the Descriptions Text for Descriptions
-        holder.NewsDescriptionText.setText(DescriptionsArrayList.get(position));
-
-
-        // Modify Image According to URL -> Picasso
-        if(isOnline(mContext)) {
-            Picasso.get().load(ImageURLArrayList.get(position)).fit().centerInside().into(holder.ImageNews);
-        }else{
-            holder.ImageNews.setImageResource(R.drawable.materialwall);
-        }
-        ChipURLLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SetOnClickListenersForUrlInBrowser(ArrayListURLValues.get(holder.getPosition()), view.getContext());
+        try {
+            // Modify The Text for Titles
+            if (TitlesArrayList.get(position) != null && TitlesArrayList.get(position) != "" && TitlesArrayList.get(position) != "null") {
+                holder.ChipNewsTitle.setText(TitlesArrayList.get(position));
+            } else {
+                holder.ChipNewsTitle.setText("This Article Has No Title");
             }
-        });
+
+            // Modify the Descriptions Text for Descriptions
+            if (DescriptionsArrayList.get(position) != null && DescriptionsArrayList.get(position) != "" && DescriptionsArrayList.get(position) != "null") {
+                holder.NewsDescriptionText.setText(DescriptionsArrayList.get(position));
+            } else {
+                holder.NewsDescriptionText.setText("This article has no description");
+            }
+
+            // Modify Image According to URL -> Picasso
+            if (isOnline(mContext)) {
+                try {
+                    Picasso.get().load(ImageURLArrayList.get(position)).fit().centerInside().into(holder.ImageNews);
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                    holder.ImageNews.setImageResource(R.drawable.materialwall); // Load Backup image
+                }
+            } else {
+                holder.ImageNews.setImageResource(R.drawable.materialwall);
+            }
+            ChipURLLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    SetOnClickListenersForUrlInBrowser(ArrayListURLValues.get(holder.getPosition()), view.getContext());
+                }
+            });
+        }catch(Exception exc){
+            System.out.println(exc.getMessage());
+            Toast.makeText(mContext.getApplicationContext(), "There Was A Problem While Displaying the News List", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
